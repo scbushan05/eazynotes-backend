d1c126e8374dfab0b2fa577ca1aa539ee2c4e01a
Bushan SC <bushan1992@zohomail.in>
2023-03-06 22:20:23 +0530

    initial commit

---
+++ src/main/java/in/bushansirgur/eazynotes/EazyNotesApplication.java
@@ -0,0 +1,13 @@
+package in.bushansirgur.eazynotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EazyNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EazyNotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/bushansirgur/eazynotes/EazyNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package in.bushansirgur.eazynotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EazyNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
