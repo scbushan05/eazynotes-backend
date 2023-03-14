3f91b88ff979efd9a99348d5f583072c6f7c6e89
Bushan SC <bushan1992@zohomail.in>
2023-03-14 19:05:00 +0530

    added cross origin

--- src/main/java/in/bushansirgur/eazynotes/controller/NotesController.java
+++ src/main/java/in/bushansirgur/eazynotes/controller/NotesController.java
@@ -3,12 +3,14 @@ package in.bushansirgur.eazynotes.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import in.bushansirgur.eazynotes.entity.Note;
 import in.bushansirgur.eazynotes.service.NotesService;
 
+@CrossOrigin("*")
 @RestController
 public class NotesController {
 	
